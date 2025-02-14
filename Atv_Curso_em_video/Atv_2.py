0b5134f0abe74584f8293753e5f7dd19d9b7286b
LittIeLove <breno.bso69@gmail.com>
2025-02-14 16:08:31 +0000

    salvando

--- Atv_Curso_em_video/Atv_2.py
+++ Atv_Curso_em_video/Atv_2.py
@@ -6,3 +6,10 @@ n2=int (input ("Write first number: "))
 print()
 sum= n1 + n2
 print("The sum of the first and second numbers is :", sum)
+
+if sum < 25:
+    print("Not Pass")
+    
+
+else:
+    print("pass")
