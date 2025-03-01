7cdc586e046e6607fba3ffcaf6202ef8a5374646
LittIeLove <breno.bso69@gmail.com>
2025-03-01 01:46:07 +0000

    Salvando

---
+++ aula6.py/desfio_1.py
@@ -0,0 +1,14 @@
+import os
+os.system ("clear")
+#Entrada de dados
+
+n1= int (input("Write first number: "))
+n2= int (input ("Write first number: "))
+
+print()
+#Saida de dados
+os.system ("clear")
+
+sum = n1 + n2
+print(f"The sum of the {n1} and {n2} is :", sum)
+print(type(sum))
---
+++ teste_type.py
@@ -0,0 +1,12 @@
+import os 
+os.system ("clear")
+#Entrada de dados
+
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite um numero: "))
+os.system ("clear")
+#saida de Dados
+s = n1 + n2
+print (f"A soma entre {n1} e {n2} = {s}")
+
+
